1857bc4bf0ca9783e6458eeac41b90758d2d6ca2
kamilamo0809 <143611705+kamilamo0809@users.noreply.github.com>
2024-09-27 11:11:59 +0200

    Data handeling: From excel file to dictionary

---
+++ Data-handeling.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+def read_previous_spot(year: int, zone: str) -> dict:
+
+    # Make dictionary which direct the year to the right excel file
+    file_dict = {2021: "spotpriser_21.xlsx",
+                 2022: "spotpriser_22.xlsx",
+                 2023: "spotpriser_23.xlsx"}
+
+    # Extract the correct file from the dictionary
+    filename = file_dict[year]
+    # Read excel
+    data = pd.read_excel(filename)
+    # Extratc numbers from the right price zone
+    data = data[zone]
+
+    # Convert to dictionary
+    price_dict = data.to_dict()
+
+    return price_dict
+
+read_previous_spot(2023, "NO5")
